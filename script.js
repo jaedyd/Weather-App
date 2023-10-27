535e207ffa1ecca7c7d68b476df707831b13c5d1
jaedyd <wodyd0313@gmail.com>
2023-10-27 07:14:28 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+const searchBox = document.querySelector(".city-input");
+const searchButton = document.querySelector(".button");
+const weathericon = document.querySelector(".weather-icon");
+
+async function getWeather(city) {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid={process.env.APIKEY}');
+
+  if(response.status == 404) {
+    document.querySelector(".weather-info").style.display = 'none';
+    document.querySelector(".error").style.display = "block";
+  }else{
+
+  }
+
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp - 273.15) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')";
+
+  weathericon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon +"@2x.png"
+
+  document.querySelector(".weather-info").style.display = "block";
+}
+
+const getWeatherButton = document.querySelector('.search button');
+getWeatherButton.addEventListener('click', () => {
+  const city = document.querySelector('.city-input').value;
+  getWeather(city);
+});
+
+
